9cbab52f121fd86e33262a9982d4ecc509527af6
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2019-07-01 20:06:41 +0300

    I's done

---
+++ DirectoryComparison/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DirectoryComparison
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDir1 = new System.Windows.Forms.TextBox();
+            this.tbDir2 = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            this.Directory1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Directory2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory 1 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Directory 2 :";
+            // 
+            // tbDir1
+            // 
+            this.tbDir1.Location = new System.Drawing.Point(103, 31);
+            this.tbDir1.Name = "tbDir1";
+            this.tbDir1.Size = new System.Drawing.Size(540, 22);
+            this.tbDir1.TabIndex = 2;
+            // 
+            // tbDir2
+            // 
+            this.tbDir2.Location = new System.Drawing.Point(103, 83);
+            this.tbDir2.Name = "tbDir2";
+            this.tbDir2.Size = new System.Drawing.Size(540, 22);
+            this.tbDir2.TabIndex = 3;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(336, 140);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(128, 58);
+            this.btnCompare.TabIndex = 4;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Directory1,
+            this.Directory2});
+            this.dgvFiles.Location = new System.Drawing.Point(103, 221);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.RowTemplate.Height = 24;
+            this.dgvFiles.Size = new System.Drawing.Size(540, 205);
+            this.dgvFiles.TabIndex = 5;
+            // 
+            // Directory1
+            // 
+            this.Directory1.HeaderText = "Directory 1";
+            this.Directory1.Name = "Directory1";
+            // 
+            // Directory2
+            // 
+            this.Directory2.HeaderText = "Directory 2";
+            this.Directory2.Name = "Directory2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFiles);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.tbDir2);
+            this.Controls.Add(this.tbDir1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Directory Comparison";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDir1;
+        private System.Windows.Forms.TextBox tbDir2;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Directory1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Directory2;
+    }
+}
+
---
+++ DirectoryComparison/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryComparison
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SizeColumns();
+        }
+
+        private void SizeColumns()
+        {
+            int wid = (int)((dgvFiles.Width - 50) / 2);
+            if (wid < 10) wid = 10;
+            dgvFiles.Columns[0].Width = wid;
+            dgvFiles.Columns[1].Width = wid;
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            dgvFiles.Rows.Clear();
+
+            string dir1 = tbDir1.Text;
+            if (!dir1.EndsWith("\\")) dir1 += "\\";
+            string[] file_names1 = Directory.GetFiles(dir1);
+            for (int i = 0; i < file_names1.Length; i++)
+            {
+                file_names1[i] = file_names1[i].Replace(dir1, "");
+            }
+            Array.Sort(file_names1);
+
+            string dir2 = tbDir2.Text;
+            if (!dir2.EndsWith("\\")) dir2 += "\\";
+            string[] file_names2 = Directory.GetFiles(dir2);
+            for (int i = 0; i < file_names2.Length; i++)
+            {
+                file_names2[i] = file_names2[i].Replace(dir2, "");
+            }
+            Array.Sort(file_names2);
+
+            int i1 = 0, i2 = 0;
+            while ((i1 < file_names1.Length) && (i2 < file_names2.Length))
+            {
+                if (file_names1[i1] == file_names2[i2])
+                {
+                    dgvFiles.Rows.Add(new Object[] { file_names1[i1], file_names2[i2] });
+                    i1++;
+                    i2++;
+                }
+                else if (file_names1[i1].CompareTo(file_names2[i2]) < 0)
+                {
+                    dgvFiles.Rows.Add(new Object[] { file_names1[i1], null });
+                    i1++;
+                }
+                else
+                {
+                    dgvFiles.Rows.Add(new Object[] { null, file_names2[i2] });
+                    i2++;
+                }
+            }
+
+            for (int i = i1; i < file_names1.Length; i++)
+            {
+                dgvFiles.Rows.Add(new Object[] { file_names1[i], null });
+            }
+
+
+            for (int i = i2; i < file_names2.Length; i++)
+            {
+                dgvFiles.Rows.Add(new Object[] { null, file_names2[i] });
+            }
+
+
+
+        }
+    }
+}
